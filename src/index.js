915392ff71249be415b2d762f5beefa554cd18bd
fai-songprasit <prapawan.songprasit@gmail.com>
2021-01-05 17:23:11 +1300

    enable login

--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
-import { BrowserRouter } from 'react-router-dom';
 // import * as serviceWorker from './serviceWorker';
-
 // 1
 import {
-  ApolloProvider,
   ApolloClient,
+  ApolloProvider,
   createHttpLink,
   InMemoryCache
-} from '@apollo/client';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
+import { AUTH_TOKEN } from './constants';
+import "./styles/index.css";
 
 // 2
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
+  uri: "http://localhost:4000",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
 });
 
 // 3
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 // 4
@@ -31,6 +42,6 @@ ReactDOM.render(
       <App />
     </ApolloProvider>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 // serviceWorker.unregister();
